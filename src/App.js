dbb1691f2c89dfd2dafd3639a264aa5bfc931fe0
yixin0829 <56566212+yixin0829@users.noreply.github.com>
2021-07-05 22:07:34 -0400

    minor change

--- src/App.js
+++ src/App.js
@@ -20,23 +20,24 @@ function App() {
       <h1>YIXIN TIAN</h1>
       <div class="icon-container">
         <SocialIcon class="icon-item" url="https://www.linkedin.com/in/yixintian/" style={{ height: 25, width: 25 }} target="_blank"/>
-        <SocialIcon class="icon-item" url="https://open.spotify.com/user/uyavk6iz1mjxx3861u59zqwc0" style={{ height: 25, width: 25 }} target="_blank"/>
-        <SocialIcon class="icon-item" url="https://www.instagram.com/yix.tian/?hl=en" style={{ height: 25, width: 25 }} target="_blank"/>
         <SocialIcon class="icon-item" url="https://github.com/yixin0829" style={{ height: 25, width: 25 }} target="_blank"/>
+        <SocialIcon class="icon-item" url="https://open.spotify.com/user/uyavk6iz1mjxx3861u59zqwc0?si=1ffcef5d3fb74132" style={{ height: 25, width: 25 }} target="_blank"/>
+        <SocialIcon class="icon-item" url="https://www.instagram.com/yix.tian/?hl=en" style={{ height: 25, width: 25 }} target="_blank"/>
+        <SocialIcon class="icon-item" url="https://www.facebook.com/yixin.tian0829" style={{ height: 25, width: 25 }} target="_blank"/>
       </div>
 
       <p>Hello!</p>
-      <p>My name is Yixin /ee'shin/ Tian (田毅鑫). I play with data and discover insights.</p>
+      <p>My name is Yixin /ee'shin/ Tian (田毅鑫). I play with data and create insights.</p>
       <p>Right now, I'm working as a Data Analyst Intern
         at <a href="https://www.kijiji.ca/kijijicentral/general/about-us/?utm_source=Kijiji&utm_medium=Footer" target="_blank">Kijiji</a>, a classifieds
         site. I dedicate my time in creating, learning things with a interest in people.</p>
       <p>I've beening studying <a href="https://www.ece.utoronto.ca/" target="_blank">Electrical and Computer Engineering</a> at University of
         Toronto since 2018 and plan to complete my bachelor's degree in
         2023 with a minor in artificial intelligence and engineering business.</p>
-      <p>Some of my long-term hobbies include <a href="https://500px.com/p/fredtian0829?view=photos">photography</a>
+      <p>Some of my long-term hobbies include <a href="https://500px.com/p/fredtian0829?view=photos" target="_blank">photography</a>
         , <a href="https://www.notion.so/fa102b35de4a45828c51b5e8f79eb8ac?v=d3792f0ac9204f7cbf873c97c5446937" target="_blank">films</a>, and travelling.
         Currently I'm interested in graph machine learning, web dev, running, wine tasting, and rock climbing.</p>
-      <p>Reach out to me and I'll be more than happy and grateful to chat with you and get know each other.</p>
+      <p>Reach out to me and I'll be more than happy and grateful to chat with you and get to know each other.</p>
       <p>Bye now <Emoji symbol="😉"/></p>
 
       <footer class="footer"><Emoji symbol="🔨"/> by Yixin</footer>
---
+++ src/README.md
@@ -0,0 +1,6 @@
+# Workflow
+
+* checkout new `dev` branch to make changes
+* commit changes and merge with `main`
+* `npm run deploy` on `main` to deploy the site
+* recreate CNAME in the setting every time after deployment (for now)
